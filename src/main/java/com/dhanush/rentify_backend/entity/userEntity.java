45a719552b5ed60e5d8a9417969cf86bab6acb2f
Dhanush653 <dk652003@gmail.com>
2026-07-10 19:30:27 +0530

    Initial commit

---
+++ src/main/java/com/dhanush/rentify_backend/entity/userEntity.java
@@ -0,0 +1,4 @@
+package com.dhanush.rentify_backend.entity;
+
+public class userEntity {
+}
---
+++ src/test/java/com/dhanush/rentify_backend/RentifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanush.rentify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
